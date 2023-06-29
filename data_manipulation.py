2a1078754c870fdae1ac6a5bf5e8985f2cb49e87
poisonshade <imildositoe@gmail.com>
2023-06-29 21:39:29 +0200

    documentation template added

---
+++ data_manipulation.py
@@ -0,0 +1,10 @@
+"""
+@author: Imildo Sitoe
+@email: imildo.sitoe@iu-study.org
+@description: this file aims to
+"""
+
+
+"""
+Use Pandas to read the training datasets (A) and ideal functions dataset (C) from CSV files using Pandas.
+"""
