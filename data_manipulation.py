e21ea2877e6fbb36134b881e2b036f595e1a336c
poisonshade <imildositoe@gmail.com>
2023-06-29 22:39:53 +0200

    function to read csv using pandas added

--- data_manipulation.py
+++ data_manipulation.py
@@ -5,6 +5,22 @@
 """
 
 
+
+# Imports
+import pandas as pd
+
+
+
 """
-Use Pandas to read the training datasets (A) and ideal functions dataset (C) from CSV files using Pandas.
+Read the training datasets (A), test dataset (B), and ideal functions dataset (C) from the CSV files.
 """
+def readFiles():
+    data = pd.read_csv("datasets/train.csv")
+    print(data)
+
+
+def main():
+    readFiles()
+
+if __name__ == "__main__":
+    main()
