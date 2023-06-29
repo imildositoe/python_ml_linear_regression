cb7a3ed00be0db04ffb61347b38be6be512d4ad0
poisonshade <imildositoe@gmail.com>
2023-06-30 00:10:29 +0200

    added the loops in the squared deviation function. stayed on removing the internal loop and replace it with a DataFrame iterator function

--- data_manipulation.py
+++ data_manipulation.py
@@ -18,14 +18,34 @@ def get_data(file_path):
     return pd.read_csv(file_path)
 
 
+"""
+Function to calculate the squared deviations of the data
+"""
+def squared_deviations():
+    train_data = get_data("datasets/train.csv")
+    test_data = get_data("datasets/test.csv")
+    ideal_data = get_data("datasets/ideal.csv")
+
+    for row in train_data.itertuples():
+        print(row[1])
+        
+
+        for j in ideal_data.index: # to remove this loop
+            pass
+            # Calculate the sum of squared deviations for each of the 50 ideal functions using the training datasets.
+            # Select the four ideal functions with the lowest sum of squared deviations.
+    
+    # Return the four selected ideal functions
+
 
 
 
 
 def main():
-    print(get_data("datasets/train.csv"))
-    print(get_data("datasets/test.csv"))
-    print(get_data("datasets/ideal.csv"))
+    # print(get_data("datasets/train.csv"))
+    # print(get_data("datasets/test.csv"))
+    # print(get_data("datasets/ideal.csv"))
+    squared_deviations()
 
 if __name__ == "__main__":
     main()
