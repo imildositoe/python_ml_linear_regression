ba8b2db6e8719e1c1490bfd12a5189a9957ec8c4
poisonshade <imildositoe@gmail.com>
2023-07-02 22:06:49 +0200

    loops for squared deviation calculation added

--- data_manipulation.py
+++ data_manipulation.py
@@ -26,14 +26,18 @@ def squared_deviations():
     test_data = get_data("datasets/test.csv")
     ideal_data = get_data("datasets/ideal.csv")
 
-    for row in train_data.itertuples():
-        print(row[1])
-        
 
-        for j in ideal_data.index: # to remove this loop
-            pass
-            # Calculate the sum of squared deviations for each of the 50 ideal functions using the training datasets.
-            # Select the four ideal functions with the lowest sum of squared deviations.
+    # Calculate the sum of squared deviations for each of the 50 ideal functions using the training datasets.
+    # Select the four ideal functions with the lowest sum of squared deviations.
+    for index, row in ideal_data.iterrows():
+        for column in ideal_data.columns:
+            value = row[column]
+            print(value)
+            # y1 = (y11 - Y1(x1))^2, (y21 - Y1(x2))^2, ..., (y1n - Y1(xn))^2
+            # y1 = sum ((y11 - Y1(x1))^2)) + ((y21 - Y1(x2))^2) + ,..., (y1n - Y1(xn))^2))
+            # y2 = sum ((y12 - Y2(x1))^2)) + ((y21 - Y1(x2))^2) + ,..., (y1n - Y1(xn))^2))
+
+            # select the 4 lowest between [y1, y2, ..., yn]
     
     # Return the four selected ideal functions
 
