f55177bbbba931a4a818641d394528f2f0616855
poisonshade <imildositoe@gmail.com>
2023-09-08 17:13:31 +0200

    main.py file added for new implementation

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+@author: Imildo Sitoe
+@email: imildo.sitoe@iu-study.org
+@description: this file aims to
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('datasets/train.csv')
+
