723d41fc3d152f7892aa2c5c0856a7d8d0571cd3
poisonshade <imildositoe@gmail.com>
2023-10-05 20:11:25 +0200

    sqlite database sketchadded

--- my_main.py
+++ my_main.py
@@ -9,18 +9,35 @@ import pandas as pd
 import sqlite3
 
 class Connection():
-    cursor = ''
 
-    #Connect to DB and create a cursor
-    def connect():
+    # Connect to DB and create a cursor
+    def database():
         conn = sqlite3.connect('data.db')
         cursor = conn.cursor()
 
-    # Write a query and execute it with cursor
-    query = 'select sqlite_version();'
-    cursor.execute(query)
+        # Write a query and execute it with cursor
+        cursor.execute('''CREATE TABLE IF NOT EXISTS data (x, y1, y2)''')
+        conn.commit()
+
+        # Test data and insert into a database
+        function_list = [
+            (1, 2, 3),
+            (3, 2, 1),
+            (2, 1, 3)
+        ]
+        cursor.executemany('''INSERT INTO data (x, y1, y2) VALUES (?, ?, ?)''', function_list)
+        conn.commit()
+
+        # Pull and display data
+        function_data = cursor.execute("SELECT * FROM data")
+        for row in function_data:
+            print(row)
+
+        # Close DB objects
+        cursor.close()
+        conn.close()
 
 
 class Data():
     def dataPreparation():
-        xx
+        x = ''
