36a34e67c3a561ca2d953fbe53209e3b429462cb
poisonshade <imildositoe@gmail.com>
2023-10-08 23:43:58 +0200

    main file with select methods added

--- main.py
+++ main.py
@@ -4,6 +4,21 @@
 @description: this file contains all python assignment task code using oop
 """
 
+from sqlalchemy.orm import sessionmaker
+import database as db
 import numpy as np
 import pandas as pd
 
+#Creating a session
+Session = sessionmaker(bind=db.engine)
+session = Session()
+
+for row in session.query(db.Train).all():
+    print(row.x, row.y1)
+
+
+# Youtube how to create a training model || check results of ChatGPT as well
+
+# Iteration with a filter case
+# for row in session.query(db.Train).filter(db.Train.x > 1):
+#     print(row)
