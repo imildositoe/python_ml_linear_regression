8f50a5323c39d2a903264f0a5767cab74e49c2ef
poisonshade <imildositoe@gmail.com>
2023-10-09 18:01:01 +0200

    training query and conversion to dataframe added

--- main.py
+++ main.py
@@ -6,12 +6,14 @@
 
 from sqlalchemy.orm import sessionmaker
 import database as db
+import data_loading
 import numpy as np
 import pandas as pd
 
 #Creating a session
 Session = sessionmaker(bind=db.engine)
 session = Session()
+# dl = data_loading
 
 
 # Iteration with a filter case
@@ -23,4 +25,12 @@ for row in session.query(db.Train).all():
     print(row.x, row.y1)
 
 
-########### Youtube how to create a training model || check results of ChatGPT as well ############
+train_data = session.query(db.Train.__table__).all()
+df = pd.DataFrame(train_data)
+x = df['x']
+y = df.drop(columns=['id', 'x'])
+
+print('-----------------------XXXXXXXXXX------------------------')
+print(x)
+print('-----------------------YYYYYYYYYY------------------------')
+print(y)
